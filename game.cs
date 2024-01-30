89ef65712d0ba1ac42393b1ced512c6ef7e184c3
Abz1717 <2457570@dundee.ac.uk>
2024-01-30 04:35:07 +0000

    took away code for two grids in public game(), and created a function to initiliazeGrid for computer and player, also ceated a shipPlacement phase to allow user to click on grid and choose ship placement before game begins.

--- game.cs
+++ game.cs
@@ -7,67 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace Battleship_Grid_Game
 {
     public partial class game : Form
     {
 
-        Button[,] btnGrid1 = new Button[4, 4];
-        Button[,] btnGrid2 = new Button[4, 4];
+        Button[,] playerGrid = new Button[4, 4];
+        Button[,] computerGrid = new Button[4, 4];
+
+
+        bool shipPlacementPhase = true; 
 
         public game()
         {
 
 
             InitializeComponent();
-
-            // placing the grid in the right place
-
-            int buttonWidth = 70;
-            int buttonHeight = 70;
-            int horizontalSpacing = 2;
-            int verticalSpacing = 2;
-            int gridX = 4;
-            int gridY = 4;
-
-            int grid1PostionX = 121;
-            int grid1PostionY = 223;
-
-            int grid2PostionX = 640;
-            int grid2PositionY = 223;
-
-
-            // first grid 
-            for (int x = 0; x < gridX; x++)
-            {
-
-                for (int y = 0; y < gridY; y++)
-                {
-                    btnGrid1[x, y] = new Button();
-                    btnGrid1[x, y].SetBounds(grid1PostionX + (buttonWidth + horizontalSpacing) * x, grid1PostionY + (buttonHeight + verticalSpacing) * y, buttonWidth, buttonHeight);
-                    btnGrid1[x, y].BackColor = Color.PowderBlue;
-
-                    Controls.Add(btnGrid1[x, y]);
-                }
-            }
-
-            // second grid
-
-            for (int x = 0; x < gridX; x++)
-            {
-                for (int y = 0; y < gridY; y++)
-                {
-                    btnGrid2[x, y] = new Button();
-                    btnGrid2[x, y].SetBounds(grid2PostionX + (buttonWidth + horizontalSpacing) * x, grid2PositionY + (buttonHeight + verticalSpacing) * y, buttonWidth, buttonHeight);
-                    btnGrid2[x, y].BackColor = Color.PowderBlue;
-                    Controls.Add(btnGrid2[x, y]);
-                }
-            }
+            InitializeGrid(playerGrid, 121, 223);
+            InitializeGrid(computerGrid, 640, 223);
 
 
 
         }
+
+        private void InitializeGrid(Button[,] grid, int startX, int startY)
+        {
+            int buttonWidth = 70;
+            int buttonHeight = 70;
+            int horizontalSpacing = 2;
+            int verticalSpacing = 2;
+
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    grid[x, y] = new Button();
+                    grid[x, y].SetBounds(startX + (buttonWidth + horizontalSpacing) * x, startY + (buttonHeight + verticalSpacing) * y, buttonWidth, buttonHeight);
+                    grid[x, y].BackColor = Color.PowderBlue;
+
+                    if (shipPlacementPhase)
+                        grid[x, y].Click += ShipPlacement_Click; // Attach a different click event handler during ship placement phase
+                    else
+                        grid[x, y].Click += GridButton_Click;
+
+                    Controls.Add(grid[x, y]);
+                }
+            }
+
+        }
+
+        private void ShipPlacement_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void GridButton_Click(object sender, EventArgs e)
+        {
+         
+
+        }
+
 
         private void game_Load(object sender, EventArgs e)
         {
