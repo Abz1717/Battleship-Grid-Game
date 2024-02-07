b34bcf7e7ac07f39c5e969642a80f44a240f378f
Tanush <tanush1912@gmail.com>
2024-02-07 21:03:50 +0000

    Trying to implement a Win/Loss ratio for the whole game

---
+++ PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public class PlayerData
+{
+	public PlayerData()
+	{
+		public int Wins { get; set; }
+		public int Losses { get; set; }
+	}
+}
---
+++ PlayerDataManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class PlayerDataManager
+{
+    private static string _filePath = "playerData.dat";
+
+    public static void SavePlayerData(PlayerData playerData)
+    {
+        try
+        {
+            using (Stream stream = File.Open(_filePath, FileMode.Create))
+            {
+                BinaryFormatter bin = new BinaryFormatter();
+                bin.Serialize(stream, playerData);
+            }
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Error saving player data");
+        }
+    }
+}
+
+public static PlayerData LoadPlayerData()
+{
+    PlayerData playerData = new PlayerData();
+    try
+    {
+        if (File.Exists(_filePath))
+        {
+            using (Stream stream = File.Open(_filePath, FileMode.Open))
+            {
+                BinaryFormatter bin = new BinaryFormatter();
+                playerData = (PlayerData)bin.Deserialize(stream);
+                return playerData;
+            }
+        }
+}
+    catch (IOException)
+    {
+    Console.WriteLine("Error loading player data" + Exception.Message);
+}
+    return new playerData;
+}
+{
+
+}
